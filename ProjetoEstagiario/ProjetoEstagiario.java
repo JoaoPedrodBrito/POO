5ec8e8f07a6722ff45f70c5503ee65f1854339b2
JoaoPedrodBrito <117210015+JoaoPedrodBrito@users.noreply.github.com>
2023-09-03 22:45:59 -0400

    Add files via upload
    CÓDIGO ATIVIDADE 04

---
+++ ProjetoEstagiario/ProjetoEstagiario.java
@@ -0,0 +1,129 @@
+// Classe abstrata Pessoa
+public abstract class Pessoa {
+    private String nome;
+    private int idade;
+    private Endereco endereco;
+
+    public Pessoa(String nome, int idade, Endereco endereco) {
+        this.nome = nome;
+        this.idade = idade;
+        this.endereco = endereco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+}
+
+// Classe Funcionario que herda de Pessoa
+public class Funcionario extends Pessoa {
+    private String cargo;
+    private double salario;
+
+    public Funcionario(String nome, int idade, Endereco endereco, String cargo, double salario) {
+        super(nome, idade, endereco);
+        this.cargo = cargo;
+        this.salario = salario;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+}
+
+// Classe Passageiro que herda de Pessoa
+public class Passageiro extends Pessoa {
+    private int numeroVoo;
+
+    public Passageiro(String nome, int idade, Endereco endereco, int numeroVoo) {
+        super(nome, idade, endereco);
+        this.numeroVoo = numeroVoo;
+    }
+
+    public int getNumeroVoo() {
+        return numeroVoo;
+    }
+
+    public void setNumeroVoo(int numeroVoo) {
+        this.numeroVoo = numeroVoo;
+    }
+}
+
+// Classe Endereco
+public class Endereco {
+    private String rua;
+    private String cidade;
+    private String estado;
+    private String cep;
+
+    public Endereco(String rua, String cidade, String estado, String cep) {
+        this.rua = rua;
+        this.cidade = cidade;
+        this.estado = estado;
+        this.cep = cep;
+    }
+
+    public String getRua() {
+        return rua;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+}
